462902e316fc1305f5ac843b091783a9dac1a872
ar-itlabs <admir.ramic@itlabs.ch>
2019-09-28 11:14:42 +0200

    refactor(menu): use semantic correct html elements

--- projects/ng-utils/src/lib/menu/menu.component.ts
+++ projects/ng-utils/src/lib/menu/menu.component.ts
@@ -3,7 +3,6 @@ import {
   Input,
   ContentChild,
   ViewEncapsulation,
-  HostBinding,
   TemplateRef,
   Output,
   EventEmitter
@@ -22,11 +21,12 @@ import { MenuItemIconDirective } from './menu-item-icon.directive';
  *
  * Following is the list of structural style classes:
  *
- * | Name               | Element                              |
- * |--------------------|--------------------------------------|
- * | itl-menu           | Host element                         |
- * | itl-menu-item      | Menu item, wrapper of icon and label |
- * | itl-menu-item-icon | Icon element of a menu item          |
+ * | Name                | Element                                                             |
+ * |---------------------|---------------------------------------------------------------------|
+ * | itl-menu            | `ul` as menu element.                                               |
+ * | itl-menu-item       | `li` as menu item, wrapper of icon and label.                       |
+ * | itl-menu-item-icon  | `span` element containing the icon of the menu item.                |
+ * | itl-menu-item-label | `span` element containing the label of the menu item.               |
  *
  *
  * @example Simplest way with e.g Font Awesome.
@@ -84,13 +84,6 @@ export class MenuComponent {
   public get menuItemIconTemplate(): TemplateRef<any> {
     return this.menuItemIcon ? this.menuItemIcon.template : null;
   }
-
-  /**
-   * Bind css class `itl-menu` to host.
-   * @hidden
-   */
-  @HostBinding('class')
-  public readonly class = 'itl-menu';
 
   /**
    * Menu's source of data, a stream that emits a data array each time the array changes.
